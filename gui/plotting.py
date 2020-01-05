b80a64444820473b87461386e3e0436362ad673e
Jan Schaffranek <jan.schaffranek@rub.de>
2020-01-05 18:51:20 +0100

    Updated to TensorFlow 2.0

--- gui/plotting.py
+++ gui/plotting.py
@@ -1,6 +1,5 @@
-# Imports
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import matplotlib.image as mpimg
 
 # Display the Digit from the image
@@ -66,11 +65,13 @@ def plot_confusion_matrix(cm, classes, normalize=False,
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
     thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            plt.text(j, i, cm[i, j],
+                    horizontalalignment="center",
+                    color="white" if cm[i, j] > thresh else "black")
 
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+    plt.show()
--- gui/preprocessing.py
+++ gui/preprocessing.py
@@ -1,47 +1,46 @@
-# Import Modules
+import os
+
 import numpy as np
 import matplotlib.pyplot as plt
+
 import cv2
 from scipy.ndimage import center_of_mass
 
-def load(filename):
-    image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+def load(image_path):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     return image
 
 def resize(image):
-    image = cv2.resize(image, (28, 28))
+    image = cv2.resize(image, (28,28))
     return image
 
 def normalize(image):
     _, image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV)
-    image = image / 255.0
     return image
 
 def center(image):
     cy, cx = center_of_mass(image)
-
     rows, cols = image.shape
     shiftx = np.round(cols/2.0-cx).astype(int)
     shifty = np.round(rows/2.0-cy).astype(int)
-
     M = np.float32([[1, 0, shiftx], [0, 1, shifty]])
     image = cv2.warpAffine(image, M, (cols, rows))
-
-    return image
-
-def correct(image):
-    image[:,0] = 0.0
-    image[:,-1] = 0.0
-    image[0,:] = 0.0
-    image[-1,:] = 0.0
     return image
 
 def get_image(DrawingFrame):
     pixmap = DrawingFrame.grab()
-    pixmap.save("image", "jpg")
-    image = load("image").astype(np.float32)
-    image = normalize(image)
-    image = correct(image)
-    image = center(image)
+    temp_image_path = os.path.join(PROJECT_PATH, "gui", "temp_image.jpg")
+    pixmap.save(temp_image_path)
+    image = load(temp_image_path).astype(np.float32)
     image = resize(image)
+    plt.imshow(image, cmap="gray")
+    plt.show()
+    image = normalize(image)
+    plt.imshow(image, cmap="gray")
+    plt.show()
+    image = center(image)
+    plt.imshow(image, cmap="gray")
+    plt.show()
     return image
